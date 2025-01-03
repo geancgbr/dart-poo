7d8a4800adbd74a5363d3bc71c06c599e98dd00e
geancarlos <geangbr@gmail.com>
2025-01-02 23:00:33 -0300

    First commit

---
+++ bin/dart_poo.dart
@@ -0,0 +1,21 @@
+void main() {
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e cítrica";
+  int diasDesdeColheita = 40;
+  bool isMadura = funcEstaMadura(diasDesdeColheita);
+  
+  print(isMadura);
+  print(funcEstaMadura(50));
+}
+
+
+bool funcEstaMadura(int dias){
+  if (dias >= 30) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
